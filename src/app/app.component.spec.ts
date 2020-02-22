b2eda666ee06ef3aad5ec0a003850c8f6cd7435a
Hvuj <56831016+Hvuj@users.noreply.github.com>
2020-02-22 20:43:56 +0200

    todoapp v1 angular9

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,33 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async } from "@angular/core/testing";
 
-describe('AppComponent', () => {
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { Todo } from "./todo";
+
+describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [FormsModule],
+      declarations: [AppComponent]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it("should create the app", async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
+  }));
 
-  it(`should have as title 'todoApp'`, () => {
+  it("should have a newTodo todo", async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('todoApp');
-  });
+    expect(app.newTodo instanceof Todo).toBeTruthy();
+  }));
 
-  it('should render title', () => {
+  it('should display "Todos" in h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('todoApp app is running!');
-  });
+    expect(compiled.querySelector("h1").textContent).toContain("Todos");
+  }));
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { TodoDataService } from "./todo-data.service";
+import { NavComponent } from './nav/nav.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, NavComponent, ContainerComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  providers: [TodoDataService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
